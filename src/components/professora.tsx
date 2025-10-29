88f37a24389fcc127d36769c87fcc3ec41cb497c
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2025-10-29 11:50:08 -0300

    Começando, instalando dependências

---
+++ src/components/professora.tsx
@@ -0,0 +1,8 @@
+
+
+export default function Professor() {
+    return(
+        <>
+        </>
+    )
+}
---
+++ src/pages/principal.tsx
@@ -0,0 +1,21 @@
+import Autor from '../components/autor'
+import Grupo from '../components/grupo'
+import { BookOpenText } from 'lucide-react'
+
+
+
+export default function Principal() {
+    return(
+        <>
+            <div className='max-w-4xl mx-auto text-center'>
+                <BookOpenText/>
+                <h1>Dom Casmurro</h1>
+                <h3>Machado de Assis</h3>
+                <h4>Um clássico da literatura brasileira</h4>
+                <button>Explore a Obra</button>
+            </div>
+            <Grupo/>
+            <Autor/>
+        </>
+    )
+}
